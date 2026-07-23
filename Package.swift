76270c0a65fac6c2050382999d854c22500d843d
psychowasp <psychowaspx@gmail.com>
2026-07-23 17:35:11 +0200

    build(package): use getDependencies() helper for package dependencies

--- Package.swift
+++ Package.swift
@@ -28,9 +28,7 @@ let package = Package(
             targets: ["NucleantSkia"]
         ),
     ],
-    dependencies: [
-        
-    ],
+    dependencies: getDependencies(),
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
